1911a26af1227055743e9bbdb3ef4109327c4041
Suhani Jagotra <suhani.jagotra07@gmail.com>
2024-07-07 13:48:23 +0530

    s

--- index.js
+++ index.js
@@ -4,34 +4,6 @@ $(".menu-btn").click(function () {
   $(".menu-btn i").toggleClass("active");
 });
 
-//for mail
-// function sendMail() {
-//   if (!(document.getElementById("topic").value == "")) {
-//     if (!(document.getElementById("name").value == "")) {
-//       if (!(document.getElementById("message").value == "")) {
-//         if (!(document.getElementById("email").value == "")) {
-//           emailjs.init("p97pgnUkvChKsPF0E");
-//           var mails = {
-//             subject: document.getElementById("topic").value,
-//             from_name: document.getElementById("name").value,
-//             message: document.getElementById("message").value,
-//             email_id: document.getElementById("email").value,
-//           };
-
-//           emailjs
-//             .send("service_pn3rj6v", "template_r2igx1k", mails)
-//             .then(function (re) {
-//               document.getElementById("alert").style.visibility = "visible";
-//               document.getElementById("topic").value = "";
-//               document.getElementById("name").value = "";
-//               document.getElementById("message").value = "";
-//               document.getElementById("email").value = "";
-//             });
-//         }
-//       }
-//     }
-//   }
-// }
 document.addEventListener("DOMContentLoaded", function () {
   emailjs.init("bMNPtUmgroIe3lr47");
 
